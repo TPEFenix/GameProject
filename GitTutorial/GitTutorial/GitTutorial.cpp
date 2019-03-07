58c729243c531f80a839f35592f512c0be24e5ac
TPEFenix <alex1107128@gmail.com>
2019-03-07 15:59:41 +0800

    尾凱

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -6,10 +6,15 @@ bool Guess(int number) {
 }
 int main(array<System::String ^> ^args)
 {
+	int §À³Í1;
+	int §À³Í2;
+	int §À³Í3;
+	int §À³Í4;
+	int §À³Í5;
+	int §À³Í6;
 	int guess;
 	do {
-		std::cout << "Choose a number between 1 - 100 :";
-		std::cin >> guess;
+		std::cout << "Choose a number between 1 - 100 :Fuck";
 	} while (!Guess(guess));
 	return 0;
 }
