2f0ef68f8ae7c244262ebc0221e2ded4c92621a1
TPEFenix <alex1107128@gmail.com>
2019-03-07 16:19:01 +0800

    8

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -3,9 +3,11 @@
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	return false;
 }
 int main(array<System::String ^> ^args)
 {
+	int §À³Í;
 	int guess;
 	do {
 		std::cout << "Choose a number between 1 - 100 :";
