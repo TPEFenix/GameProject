394f881e450ea518b56f8ec1debcd1cbea13c40c
TPEFenix <alex1107128@gmail.com>
2019-04-26 23:55:48 +0800

    Rina全基礎動作完成

--- WKFighter/Source/FunctionUser.cpp
+++ WKFighter/Source/FunctionUser.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "stdafx.h"
 #include <ddraw.h>
 #include <windows.h>
@@ -21,37 +22,37 @@ namespace FunctionUser_namespace
 
 
 
-	void LoadingResource(void (LoadingFunction)(), thread *mThread, bool *started, bool *finished)
-	{
-		if (*started == false && *finished == false)
-		{
-			*started = true;
-			*mThread = thread(LoadingFunction);
-		}
-		if (*finished == true && *started == true)
-		{
-			*started = false;
-			mThread->join();
-		}
-	}
+    void LoadingResource(void (LoadingFunction)(), thread *mThread, bool *started, bool *finished)
+    {
+        if (*started == false && *finished == false)
+        {
+            *started = true;
+            *mThread = thread(LoadingFunction);
+        }
+        if (*finished == true && *started == true)
+        {
+            *started = false;
+            mThread->join();
+        }
+    }
 
 
-	void PlayEffect(BattlePlayer *Master, string EffectName, CameraPosition Camera, double XR, double XL, double Y)
-	{
-		Master->Effects.BootEffect(&Master->Effects.Content[EffectName], Camera, XR, XL, Y, 0, 0, false, Master->IsRight);
-	}
-	void PlayEffect(EffectSprite *Effects, string EffectName, CameraPosition Camera, double XR, double XL, double Y, bool IsRight)
-	{
-		Effects->BootEffect(&Effects->Content[EffectName], Camera, XR, XL, Y, 0, 0, false, IsRight);
-	}
-	void DisableEffect(BattlePlayer * Master, string EffectName)
-	{
-		Master->Effects.Content[EffectName].loop = false;
-		Master->Effects.Content[EffectName].visable = false;
-	}
-	void DisableEffect(EffectSprite * Effects, string EffectName)
-	{
-		Effects->Content[EffectName].loop = false;
-		Effects->Content[EffectName].visable = false;
-	}
+    void PlayEffect(BattlePlayer *Master, string EffectName, CameraPosition Camera, double XR, double XL, double Y)
+    {
+        Master->Effects.BootEffect(&Master->Effects.Content[EffectName], Camera, XR, XL, Y, 0, 0, false, Master->IsRight);
+    }
+    void PlayEffect(EffectSprite *Effects, string EffectName, CameraPosition Camera, double XR, double XL, double Y, bool IsRight)
+    {
+        Effects->BootEffect(&Effects->Content[EffectName], Camera, XR, XL, Y, 0, 0, false, IsRight);
+    }
+    void DisableEffect(BattlePlayer * Master, string EffectName)
+    {
+        Master->Effects.Content[EffectName].loop = false;
+        Master->Effects.Content[EffectName].visable = false;
+    }
+    void DisableEffect(EffectSprite * Effects, string EffectName)
+    {
+        Effects->Content[EffectName].loop = false;
+        Effects->Content[EffectName].visable = false;
+    }
 }
